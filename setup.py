6f1de0dbc1c48eb911c5e0ca30ac6bf4093c86eb
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2020-04-19 18:02:24 +0200

    Added few details

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='niiview',
+      version='1',
+      description='A tool for displaying niftis in xterminals.',
+      author='Tobias Kadelka',
+      author_email='t.kadelka@fz-juelich.de',
+      packages=find_packages(),
+      license='LICENSE.txt',
+      install_requires=[
+      'nibabel',
+      'matplotlib',
+      'numpy',
+      'libsixel-python'
+	   ]
+)
