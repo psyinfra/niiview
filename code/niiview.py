5291d0661d3de79257a239ff1d789dcb59519a48
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2020-04-19 17:37:15 +0200

    Usable version

--- code/niiview.py
+++ code/niiview.py
@@ -12,165 +12,59 @@ from PIL import Image
 from io import BytesIO
 
 
+# plot the selected slices
+def create_plot():
 
-# This function creates the figure, that should be plotted later.
-# It is based on: _plot_nifti_preview(iFile, show, return_fig=False, dpi=150)
-# from https://github.com/vnckppl/niipre"""
-def create_plot( show ):
-
-    # Orientation
-    qfX = nifti_image.get_qform()[0, 0]
-    sfX = nifti_image.get_sform()[0, 0]
-
-    if qfX < 0 and (sfX == 0 or sfX < 0):
-        oL = 'R'
-        oR = 'L'
-    elif qfX > 0 and (sfX == 0 or sfX > 0):
-        oL = 'L'
-        oR = 'R'
-    else:
-        oL = ''
-        oR = ''
-
-    if sfX < 0 and (qfX == 0 or qfX < 0):
-        oL = 'R'
-        oR = 'L'
-    elif sfX > 0 and (qfX == 0 or qfX > 0):
-        oL = 'L'
-        oR = 'R'
-    else:
-        oL = ''
-        oR = ''
-
-    fig_jpeg = plt.figure(
-        facecolor='black',
-        figsize=(5, 4),
-        dpi=dpi
-    )
-	
-    # Coronal
-    ax1 = fig_jpeg.add_subplot(2, 2, 1)
-    imgplot = plt.imshow(
-        np.rot90(nifti_data[:, show[1], :]),
+    # coronal
+    ax1.imshow(
+        nifti_data[:, show[1], :],
         aspect=nifti_image.header['pixdim'][3] / nifti_image.header['pixdim'][1],
-    )
-    imgplot.set_cmap('gray')
+    ).set_cmap('gray')
 
-    ax1.hlines(nifti_data.shape[2] / 2.0, 0, nifti_data.shape[0], colors='red', linestyles='dotted', linewidth=.5)
-    ax1.vlines(nifti_data.shape[0] / 2.0, 0, nifti_data.shape[2], colors='red', linestyles='dotted', linewidth=.5)
-
-    plt.axis('off')
-
-    # Sagittal
-    ax2 = fig_jpeg.add_subplot(2, 2, 2)
-    imgplot = plt.imshow(
-        np.rot90(nifti_data[show[0], :, :]),
+    # sagittal
+    ax2.imshow(
+        nifti_data[show[0], :, :],
         aspect=nifti_image.header['pixdim'][3] / nifti_image.header['pixdim'][2],
-    )
-    imgplot.set_cmap('gray')
+    ).set_cmap('gray')
 
-    ax2.hlines(nifti_data.shape[2] / 2.0, 0, nifti_data.shape[1], colors='red', linestyles='dotted', linewidth=.5)
-    ax2.vlines(nifti_data.shape[1] / 2.0, 0, nifti_data.shape[2], colors='red', linestyles='dotted', linewidth=.5)
-
-    plt.axis('off')
-
-    # Axial
-    ax3 = fig_jpeg.add_subplot(2, 2, 3)
-    imgplot = plt.imshow(
-        np.rot90(nifti_data[:, :, show[2]]),
+    # axial
+    ax3.imshow(
+        nifti_data[:, :, show[2]],
         aspect=nifti_image.header['pixdim'][2] / nifti_image.header['pixdim'][1]
-    )
-    imgplot.set_cmap('gray')
-
-    ax3.hlines(nifti_data.shape[1] / 2.0, 0, nifti_data.shape[0], colors='red', linestyles='dotted', linewidth=.5)
-    ax3.vlines(nifti_data.shape[0] / 2.0, 0, nifti_data.shape[1], colors='red', linestyles='dotted', linewidth=.5)
-
+    ).set_cmap('gray')
+    ax4.clear()
     plt.axis('off')
-
-
-    plt.text(-10, show[1] + 5, oL, fontsize=9, color='red')  # Label on left side
-
-    # Textual information
-    # sform code
-    sform = np.round(nifti_image.get_sform(), decimals=2)
-    sform_txt = str(sform).replace('[', ' ').replace(']', ' ').replace(' ', '   ').replace('   -', '  -')
-
-    # qform code
-    qform = np.round(nifti_image.get_qform(), decimals=2)
-    qform_txt = str(qform).replace('[', ' ').replace(']', ' ').replace(' ', '   ').replace('   -', '  -')
-
-    # Dimensions
-    dims = str(nifti_data.shape).replace(', ', ' x ').replace('(', '').replace(')', '')
-    dim = ("Dimensions: " + dims)
-
-    # Spacing
-    spacing = ("Spacing: "
-               + str(np.round(nifti_image.header['pixdim'][1], decimals=2))
-               + " x "
-               + str(np.round(nifti_image.header['pixdim'][2], decimals=2))
-               + " x "
-               + str(np.round(nifti_image.header['pixdim'][3], decimals=2))
-               + " mm"
-               )
-
-    # Data type
-    type = nifti_image.header.get_data_dtype()
-    type_str = ("Data type: " + str(type))
-
-    # Volumes
-    volumes = ("Volumes: " + str(nifti_image.header['dim'][4]))
-
-    # Range
-    min = np.round(np.amin(nifti_data), decimals=2)
-    max = np.round(np.amax(nifti_data), decimals=2)
-    range = ("Range: " + str(min) + " - " + str(max))
-
-    text = (
-            dim + "\n"
-            + spacing + "\n"
-            + volumes + "\n"
-            + type_str + "\n"
-            + range + "\n\n"
-            + "sform code:\n"
-            + sform_txt + "\n"
-            + "\nqform code:\n"
-            + qform_txt
-    )
-
-    # Plot text subplot
-    ax4 = fig_jpeg.add_subplot(2, 2, 4)
-    plt.text(
-        0.15,
-        0.95,
-        text,
-        horizontalalignment='left',
-        verticalalignment='top',
-        size=6,
-        color='white',
-    )
-    plt.axis('off')
-    # Adjust whitespace
-    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+    ax4.text( 0.15, 0.95, get_text(), horizontalalignment='left', verticalalignment='top', size=6, color='white')
+    #plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
 
 
 
-# this function displays the nifti, reads the keys and changes the displayed image.
-# if a not-arrow-key is pressed by the user, the program exits this function
+# build information
+def get_text():
+    return str(
+            "(W/S) -> " + str(show[1]+1) + "/" + str(nifti_data.shape[1]) + " left" + "\n"
+          + "(E/D) -> " + str(show[0]+1) + "/" + str(nifti_data.shape[2]) + " right" + "\n"
+          + "(Q/A) -> " + str(show[2]+1) + "/" + str(nifti_data.shape[2]) + " down" + "\n"
+          )
+
+
+
+    dimensions = str()             + " x " +  str(nifti_data.shape[1])            + " x " + str(nifti_data.shape[2])
+    voxelsize  = str(nifti_image.header['pixdim'][1]) + " x " + str(nifti_image.header['pixdim'][2]) + " x, " + str(nifti_image.header['pixdim'][3]) + " mm"
+    return str( "Dimensions : "  + dimensions + "\n"
+              + "Voxel-size : "  + voxelsize + "\n"
+              + "Time-points: " + str(nifti_image.header['dim'][4]) + "\n" )
+
+
+
+# this function reads the keyboard and calls the function for creating the image.
 def display_nifti():
-
-    # selects middle slices as default vaulues for the koordinates 
-    x = int(nifti_data.shape[0]/2)
-    y = int(nifti_data.shape[1]/2)
-    z = int(nifti_data.shape[2]/2)
-
+    # enable changing of global nifti-data for changing point in time
+    global nifti_data
     # create, open and show nifti in terminal
     while( True ):
-
-        # clear the display, so the image is on top of terminal
-        os.system("clear")
-
         # create a jpg from this koordinates of the nifti
-        create_plot( [ x, y, z ] )
+        create_plot( )
         plt.savefig( tmp_nifti_file_name )
 
         #read jpg, display it, close it
@@ -182,31 +76,66 @@ def display_nifti():
         dither = sixel_dither_new(256)
         sixel_dither_initialize(dither, data, width, height, SIXEL_PIXELFORMAT_RGB888)
         sixel_encode(data, width, height, 1, dither, output)
+        os.system("clear")
         print(s.getvalue().decode('ascii'))
         image.close()
 
         # wait for user to press a key. For arrows, change displayed slice.
         # "enter"-key exit the function
         key = getkey()
-        if key == keys.UP:
-            x = x + 10
-        elif key == keys.DOWN:
-            x = x - 10
-        elif key == 's':
-            y = y + 10
-        elif key == 'x':
-            y = y - 10
-        elif key == keys.LEFT:
-            z = z + 10
-        elif key == keys.RIGHT:
-            z = z - 10
+        # change up-right
+        if key in ['e']:
+            if show[0] + 10 >= nifti_data.shape[0]:
+                show[0] = nifti_data.shape[0]-1
+            else:
+                show[0] = show[0] + 10
+        elif key in ['d']:
+            if show[0] - 10 <= 0:
+                show[0] = 0
+            else:
+                show[0] = show[0] - 10
+        # change up-left
+        elif key in ['w']:
+            if show[1] + 10 >= nifti_data.shape[1]:
+                show[1] = nifti_data.shape[1]-1
+            else:
+                show[1] = show[1] + 10
+        elif key in ['s']:
+            if show[1] - 10 <= 0:
+                show[1] = 0
+            else:
+                show[1] = show[1] - 10
+        # change down-left
+        elif key in ['q']:
+            if show[2] + 10 >= nifti_data.shape[2]:
+                show[2] = nifti_data.shape[2]-1
+            else:
+                show[2] = show[2] + 10
+        elif key in ['a']:
+            if show[2] - 10 <= 0:
+                show[2] = 0
+            else:
+                show[2] = show[2] - 10
+        # change point in time
+        elif key in ['t']:
+                if fourth_d == -1:
+                    continue
+                if fourth_d + 1 < nifti_data.shape[4]:
+                    nifti_data = nifti_image.get_data()[:, :, :, fourth_d+1 ]
+        elif key in ['g']:
+                if fourth_d == -1:
+                    continue
+                if fourth_d - 1 >= 0:
+                    nifti_data = nifti_image.get_data()[:, :, :, fourth_d-1 ]
         else:
             return
 
 
-
+##### TODO: Check 4d-function, it was never tried!!!!!!!!!!!!!
+#####
 # the mainfunction of the niiview-tool
 def main():
+
     #### parse arguments from terminal ####
     parser = argparse.ArgumentParser(description="Generate a nifti image in the terminal.")
     parser.add_argument("nifti_file")
@@ -215,18 +144,19 @@ def main():
     args = parser.parse_args()
 
     #### set global vars ####
-    global nifti_image, nifti_data, tmp_nifti_file_name, dpi#, fig_jpeg
+    global nifti_image, nifti_data, tmp_nifti_file_name, fig_jpeg, ax1, ax2, ax3, ax4, show, fourth_d
     # Try to load the nifti-data from arguments and check for errors
     nifti_image = nb.load( args.nifti_file )
     # get 3D-image (first time point) in case of 4D-input
     if nifti_image.header['dim'][0] == 3:
         nifti_data = nifti_image.get_data()
+        # no time points given
+        fourth_d = -1
     elif nifti_image.header['dim'][0] == 4:
         nifti_data = nifti_image.get_data()[:, :, :, 0]
+        fourth_d = 0
     # name of a temporary jpg-file where data is written into, so libsixel can read it
     tmp_nifti_file_name = ".nifti.jpg"
-    # size of nifti in terminal
-    dpi=args.dpi
     # Disable Toolbar for plots
     plt.rcParams['toolbar'] = 'None'
     # Set rounding
@@ -235,14 +165,20 @@ def main():
     nifti_data[np.isnan(nifti_data)] = 0
     # Black background
     plt.style.use('dark_background')
-
-    # Plot main window
-#    fig_jpeg = plt.figure(
-#        facecolor='black',
-#        figsize=(5, 4),
-#        dpi=dpi
-#    )
-
+    # build main window
+    fig_jpeg = plt.figure( facecolor='black', figsize=(5, 4), dpi=args.dpi )
+    # add axes
+    ax1 = fig_jpeg.add_subplot(2, 2, 1)
+    plt.axis('off')
+    ax2 = fig_jpeg.add_subplot(2, 2, 2)
+    plt.axis('off')
+    ax3 = fig_jpeg.add_subplot(2, 2, 3)
+    plt.axis('off')
+    ax4 = fig_jpeg.add_subplot(2, 2, 4)
+    plt.axis('off')
+    show = []
+    # selects middle slices as default vaulues for the koordinates 
+    show = [ int(nifti_data.shape[0]/2), int(nifti_data.shape[1]/2) , int(nifti_data.shape[2]/2) ]
 
     #### open/show the image ####
     display_nifti()
