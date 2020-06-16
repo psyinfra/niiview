4bee98c37c48eb0f13ba2f23e42ca3e2e55105c4
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2020-06-16 11:36:00 +0200

    Updated names and help

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
-
+''' setup file for installation of niiview '''
 from setuptools import setup, find_packages
 
 setup(
-      name='niiview',
-      version='1.0.0',
-      description='Display NIfTI images in sixel-aware terminals.',
-      author='Tobias Kadelka',
-      author_email='t.kadelka@fz-juelich.de',
-      packages=find_packages(),
-      license='ISC',
-      install_requires=[
-          'nibabel',
-          'matplotlib',
-          'numpy',
-          'libsixel-python',
-          'getkey',
-          'Pillow'
-      ],
-      python_requires = ">=3.0",
-      scripts=[
-          'niiview'
-      ],
-      data_files = [
-          ('share/man/man1', ['niiview.1'])
-      ]
+    name='niiview',
+    version='1.0.0',
+    description='Display NIfTI images in sixel-aware terminals.',
+    author='Tobias Kadelka',
+    author_email='t.kadelka@fz-juelich.de',
+    packages=find_packages(),
+    license='ISC',
+    install_requires=[
+        'nibabel',
+        'matplotlib',
+        'numpy',
+        'libsixel-python',
+        'getkey',
+        'Pillow'
+    ],
+    python_requires=">=3.0",
+    scripts=[
+        'niiview'
+    ],
+    data_files=[
+        ('share/man/man1', ['niiview.1'])
+    ]
 )
